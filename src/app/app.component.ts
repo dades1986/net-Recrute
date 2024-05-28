33c7c6723f5093b598bdf67bceaad6cabe0eac73
oumina <e.oumina@gmail.com>
2024-05-28 12:05:05 +0200

    One-Way Data Binding, Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'netrecrute';
+  user = {
+    name: 'John Doe',
+    age: 30
+  };
+  currentDate = new Date();
+  getWelcomeMessage() {
+    return 'Welcome to the Angular tutorial!';
+  }
 }
