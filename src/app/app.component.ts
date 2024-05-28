583b7afb64a97616f946db354bca671a6c6fe0e0
oumina <e.oumina@gmail.com>
2024-05-28 12:44:21 +0200

    One-Way Data Binding, Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,5 +18,8 @@ export class AppComponent {
   currentDate = new Date();
   getWelcomeMessage() {
     return 'Welcome to the Angular tutorial!';
-  }
+  };
+  //Component Property
+  imageUrlLogoNetRecrute: string = 'https://static.wixstatic.com/media/4cd9a7_2ef41cb15d5b4a5b9f69fe091d090fb3~mv2.png/v1/fill/w_72,h_56,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/smartvector_layers_collection_0005_ICON-color.png';
+  imageUrlLogoNetRecrute2: string = 'assets/images/image.png';
 }
